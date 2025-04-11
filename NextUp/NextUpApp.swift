6245d1c66f5b0960c36d674a7962194f6878bba3
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2025-04-11 07:55:25 -0500

    Initial Commit

---
+++ NextUp/NextUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextUpApp.swift
+//  NextUp
+//
+//  Created by Tamilore Oladejo on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NextUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
