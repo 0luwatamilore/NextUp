111450c242f8b74ca837795a2a38600b5e309d7e
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2025-04-12 14:35:55 -0500

    finished signup page ui

---
+++ NextUp/ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  NextUp
+//
+//  Created by Tamilore Oladejo on 4/12/25.
+//
+
+import Foundation
---
+++ NextUp/Views/SignUpView.swift
@@ -0,0 +1,50 @@
+//
+//  SignUpView.swift
+//  NextUp
+//
+//  Created by Tamilore Oladejo on 4/11/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @AppStorage("isLoggedIn") var isLoggedIn: Bool = false
+    @State private var errorMessage: String = ""
+    @State private var firstname: String = ""
+    @State private var lastname: String = ""
+    @State private var email: String = ""
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("Sign Up").font(.largeTitle).bold().padding()
+            
+            TextField("Enter your firstname", text: $firstname)
+                .autocapitalization(.none).padding()
+                .background(Color.gray.opacity(0.2)).cornerRadius(10).padding(.vertical, 5)
+            TextField("Enter your lastname", text: $lastname)
+                .autocapitalization(.none).padding()
+                .background(Color.gray.opacity(0.2)).cornerRadius(10).padding(.vertical, 5)
+            TextField("Enter your email", text: $email)
+                .autocapitalization(.none).padding()
+                .background(Color.gray.opacity(0.2)).cornerRadius(10).padding(.vertical, 5)
+            TextField("Enter your username", text: $username)
+                .autocapitalization(.none).padding()
+                .background(Color.gray.opacity(0.2)).cornerRadius(10).padding(.vertical, 5)
+            TextField("Enter your password", text: $password)
+                .autocapitalization(.none).padding()
+                .background(Color.gray.opacity(0.2)).cornerRadius(10).padding(.vertical, 5)
+            HStack {
+                Button("Register"){}.padding().font(.title2)
+            }.buttonStyle(.bordered)
+            
+            Text("fnfnf")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SignUpView()
+}
